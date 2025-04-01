bd6a12588d939df162d16a97e04b10c33c753ed0
Jannik Lohaus <jannik@lo.haus>
2025-04-01 06:47:51 +0200

    Add /chess link to main menu

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/chess">Chess</RouterLink>
       </nav>
     </div>
   </header>
