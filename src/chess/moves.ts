ccb2d5eadc2b778852b4b71e6b4eea33e5551508
Jannik Lohaus <jannik@lo.haus>
2025-04-02 01:59:35 +0200

    Fix chess/moves: JumpOnFriend missed a bit

--- src/chess/moves.ts
+++ src/chess/moves.ts
@@ -23,25 +23,25 @@
 export const WalkOnEmpty = 0b000001
 
 /**
- * Move by walking if target is friend.
- *
- * @constant {number} WalkOnFriend - Move to a friend's tile
- */
-export const WalkOnFriend = 0b000010
-
-/**
  * Move by walking if target is enemy.
  *
  * @constant {number} WalkOnEnemy - Move to an enemy's tile
  */
-export const WalkOnEnemy = 0b000100
+export const WalkOnEnemy = 0b000010
+
+/**
+ * Move by walking if target is friend.
+ *
+ * @constant {number} WalkOnFriend - Move to a friend's tile
+ */
+export const WalkOnFriend = 0b000100
 
 /**
  * Move by walking to any target (friend, enemy or empty tile).
  *
  * @constant {number} WalkOnAny - Move to any target (friend, enemy or empty tile)
  */
-export const WalkOnAny = WalkOnEmpty | WalkOnFriend | WalkOnEnemy
+export const WalkOnAny = WalkOnEmpty | WalkOnEnemy | WalkOnFriend
 
 /**
  * Move by jumping if target is empty.
@@ -51,25 +51,25 @@ export const WalkOnAny = WalkOnEmpty | WalkOnFriend | WalkOnEnemy
 export const JumpOnEmpty = 0b001000
 
 /**
- * Move by jumping if target is friend.
- *
- * @constant {number} JumpOnFriend - Jump to a friend's tile
- */
-export const JumpOnFriend = 0b010000
-
-/**
  * Move by jumping if target is enemy.
  *
  * @constant {number} JumpOnEnemy - Jump to an enemy's tile
  */
-export const JumpOnEnemy = 0b10000
+export const JumpOnEnemy = 0b010000
+
+/**
+ * Move by jumping if target is friend.
+ *
+ * @constant {number} JumpOnFriend - Jump to a friend's tile
+ */
+export const JumpOnFriend = 0b100000
 
 /**
  * Move by jumping on any target (friend, enemy or empty tile).
  *
  * @constant {number} JumpOnAny - Jump to any target (friend, enemy or empty tile)
  */
-export const JumpOnAny = JumpOnEmpty | JumpOnFriend | JumpOnEnemy
+export const JumpOnAny = JumpOnEmpty | JumpOnEnemy | JumpOnFriend
 
 /**
  * All movement types combined.
