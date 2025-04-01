e52584783eb26eb146ca361611134d250e64ecd3
Jannik Lohaus <jannik@lo.haus>
2025-04-01 06:26:05 +0200

    Fix e2e tests

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,8 +1,13 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.locator('h1')).toHaveText('You did it!');
+  await page.goto('/')
+  await expect(page.locator('h1')).toHaveText('Chess Game Application')
+})
+
+test('visits the chess-game url', async ({ page }) => {
+  await page.goto('/chess')
+  await expect(page.locator('h2')).toHaveText('Chess View')
 })
