88b1c6052cda701dd27b161a8942ae9058f387fe
Jannik Lohaus <jannik@lo.haus>
2025-04-08 11:26:39 +0200

    Improve text on HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,12 @@
 
     <section class="content">
       <p>Hello there! Welcome to my chess game application.</p>
+      <p>Here you can play a game of chess against other players.</p>
+      <p>
+        For now only local games are available, in the next releases, I plan to add both ai
+        opponents and online multiplayer functionality.
+      </p>
+      <p>Enjoy your gaming experience!</p>
     </section>
   </main>
 </template>
